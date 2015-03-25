942db193563fa86b804ba94679b8dc9cdfae6c47
apatheiagit <apatheiar@mail.ru>
2015-03-25 22:12:57 +0300

    done

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,20 @@
 var gulp = require('gulp'),
-connect = require('gulp-connect');
-opn = require('opn');
+	connect = require('gulp-connect'),
+	opn = require('opn'),
+	wiredep = require('wiredep').stream,
+	useref = require('gulp-useref'),
+	gulpif = require('gulp-if'),
+	uglify = require('gulp-uglify'),
+	minifyCss = require('gulp-minify-css');
+
+// bower
+gulp.task('wiredep', function () { 
+	gulp.src('app/*.html')
+	.pipe(wiredep({
+		directory: 'app/bower_components'
+	}))
+	.pipe(gulp.dest('app'));
+});
 
 // Запуск сервера
 gulp.task('connect', function() {
@@ -24,3 +38,16 @@ gulp.task('watch', function () {
 
 // Задача по умолчанию
 gulp.task('default', ['connect', 'watch']);
+
+// build
+gulp.task('dist', function () {
+	var assets = useref.assets();
+
+	return gulp.src('app/*.html')
+	.pipe(assets)
+	.pipe(gulpif('*.js', uglify()))
+	.pipe(gulpif('*.css', minifyCss()))
+	.pipe(assets.restore())
+	.pipe(useref())
+	.pipe(gulp.dest('dist'));
+});
